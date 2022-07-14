68648d39dea4faa74391279c916d32b6c6a96dd9
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-14 20:32:35 -0300

    feat: define todas as rotas para renderizar só o nome

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,15 +1,14 @@
-
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Login from './pages/login';
-import Register from './pages/register';
-import Saller from './pages/saller';
-import Customers from './paCes/customers';
-import Admin from './pages/admin';
-import Products from './pages/products'
-import CustomerOrders from './pages/customerOrders'
-import Checkout from './pages/checkout'
-import AllOrders from './pages/allOrders'
+import Login from './pages/login/index.jsx';
+// import Register from './pages/register';
+// import Saller from './pages/saller';
+// import Customers from './paCes/customers';
+// import Admin from './pages/admin';
+// import Products from './pages/products'
+// import CustomerOrders from './pages/customerOrders'
+// import Checkout from './pages/checkout'
+// import AllOrders from './pages/allOrders'
 
 export default class Routes extends Component {
   render() {
@@ -21,14 +20,14 @@ export default class Routes extends Component {
               <Redirect to="/login" />
             </Route>
             <Route exact path="/login" component={ Login } />
-            <Route exact path="/register" component={ Register } />
+            {/* <Route exact path="/register" component={ Register } />
             <Route exact path="/customer" component={ Customers } />
             <Route exact path="/customer/products" component={ Products } />
             <Route exact path="/customer/orders" component={ CustomerOrders } />
             <Route exact path="/customer/checkout" component={ Checkout } />
             <Route exact path="/saller" component={ Saller } />
             <Route exact path="/saller/orders" component={ AllOrders } />
-            <Route exact path="/admin" component={ Admin } />
+            <Route exact path="/admin" component={ Admin } /> */}
           </Switch>
         </main>
       </div>
