5e2c7821a16579294907faa490708b7957bce9ad
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-14 19:27:42 -0300

    fix: comenta todas as rotas até implementação de pages.

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,37 +1,37 @@
 
-import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-// import Login from './pages/login';
-// import Register from './pages/register';
-// import Saller from './pages/saller';
-// import Customers from './paCes/customers';
-// import Admin from './pages/admin';
-// import Products from ''
-// import CustomerOrders from ''
-// import Checkout from ''
-// import AllOrders from ''
+// import React, { Component } from 'react';
+// import { Route, Switch, Redirect } from 'react-router-dom';
+// // import Login from './pages/login';
+// // import Register from './pages/register';
+// // import Saller from './pages/saller';
+// // import Customers from './paCes/customers';
+// // import Admin from './pages/admin';
+// // import Products from './pages/products'
+// // import CustomerOrders from './pages/customerOrders'
+// // import Checkout from './pages/checkout'
+// // import AllOrders from './pages/allOrders'
 
-export default class Routes extends Component {
-  render() {
-    return (
-      <div>
-        <main>
-          {/* <Switch>
-            <Route exact path="/">
-              <Redirect to="/login" />
-            </Route>
-            <Route exact path="/login" component={ Login } />
-            <Route exact path="/register" component={ Register } />
-            <Route exact path="/customer" component={ Customers } />
-            <Route exact path="/customer/products" component={ Products } />
-            <Route exact path="/customer/orders" component={ CustomerOrders } />
-            <Route exact path="/customer/checkout" component={ Checkout } />
-            <Route exact path="/saller" component={ Saller } />
-            <Route exact path="/saller/orders" component={ AllOrders } />
-            <Route exact path="/admin" component={ Admin } />
-          </Switch> */}
-        </main>
-      </div>
-    );
-  }
-}
+// export default class Routes extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <main>
+//           <Switch>
+//             <Route exact path="/">
+//               <Redirect to="/login" />
+//             </Route>
+//             <Route exact path="/login" component={ Login } />
+//             <Route exact path="/register" component={ Register } />
+//             <Route exact path="/customer" component={ Customers } />
+//             <Route exact path="/customer/products" component={ Products } />
+//             <Route exact path="/customer/orders" component={ CustomerOrders } />
+//             <Route exact path="/customer/checkout" component={ Checkout } />
+//             <Route exact path="/saller" component={ Saller } />
+//             <Route exact path="/saller/orders" component={ AllOrders } />
+//             <Route exact path="/admin" component={ Admin } />
+//           </Switch>
+//         </main>
+//       </div>
+//     );
+//   }
+// }
