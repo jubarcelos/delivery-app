aaeb22580844bcfa388611a46353c4cfe996d4ed
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-15 17:44:00 -0300

    feat: esqueleto do login

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-  return (<h1>login</h1>);
+  // const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // descrever a função que transforma em hash
+
+  const setDisabled = () => {
+    const SIX = 6;
+    if (email.includes('@') && email.includes('.com') && password.length > SIX) {
+      return false;
+    }
+    return true;
+  };
+
+  const saveStorageLogin = (event) => {
+    event.preventDefault();
+    // localStorage
+    //   .setItem('customerOrder', JSON.stringify([products]));
+    // localStorage.setItem('user', JSON.stringify({ email }));
+    // localStorage.setItem('endereço', JSON.stringify({ email }));
+    // history.push('/foods');
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">
+        Login
+        <input
+          type="text"
+          data-testid="email-input"
+          name="email"
+          id="email"
+          placeholder="Digite seu Email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          data-testid="password-input"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Digite sua senha"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+      </label>
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        onClick={ saveStorageLogin }
+        disabled={ setDisabled() }
+      >
+        Enter
+      </button>
+    </form>
+  );
 }
 
 export default Login;
