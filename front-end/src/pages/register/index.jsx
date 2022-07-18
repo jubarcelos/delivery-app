820b95a013d45412d18861f455e0f9a36bf375a3
Vanessa <vanessamrios@gmail.com>
2022-07-18 11:27:06 +0200

    feat: cria o componente register

--- front-end/src/pages/register/index.jsx
+++ front-end/src/pages/register/index.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
-function register() {
-  return (<h1> register</h1>);
+class Register extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Cadastro</h1>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="nome"
+            // value={ nome }
+            data-testid="6"
+            placeholder="Seu nome"
+            // onChange={ this.handleChange }
+          />
+          <input
+            type="text"
+            name="email"
+            // value={ email }
+            data-testid="7"
+            placeholder="seu-email@site.com.br"
+            // onChange={ this.handleChange }
+          />
+          <input
+            type="text"
+            name="senha"
+            // value={ password }
+            data-testid="password-input"
+            placeholder="******"
+            // onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            // disabled={ !this.validate(email, password) }
+            // onClick={ this.handleClick }
+          >
+            CADASTRAR
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default register;
+export default Register;
