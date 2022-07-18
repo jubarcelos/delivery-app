613b6e181346695c49efed4fb1ac8f1c9a1317d0
Vanessa <vanessamrios@gmail.com>
2022-07-18 12:31:40 +0200

    feat: adiciona validacoes para nome, email e senha no componente register

--- front-end/src/pages/register/index.jsx
+++ front-end/src/pages/register/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
 class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nome: '',
+      email: '',
+      senha: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  validateEmailFormat(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  validate(nome, email, senha) {
+    const minPasswordSize = 6;
+    const minNameSize = 12;
+    if (this.validateEmailFormat(email)
+    && senha.length >= minPasswordSize
+    && nome.length >= minNameSize) {
+      return true;
+    }
+  }
+
   render() {
+    const { nome, email, senha } = this.state;
     return (
       <div>
         <div>
@@ -11,30 +44,30 @@ class Register extends React.Component {
           <input
             type="text"
             name="nome"
-            // value={ nome }
+            value={ nome }
             data-testid="6"
             placeholder="Seu nome"
-            // onChange={ this.handleChange }
+            onChange={ this.handleChange }
           />
           <input
             type="text"
             name="email"
-            // value={ email }
+            value={ email }
             data-testid="7"
             placeholder="seu-email@site.com.br"
-            // onChange={ this.handleChange }
+            onChange={ this.handleChange }
           />
           <input
             type="text"
             name="senha"
-            // value={ password }
+            value={ senha }
             data-testid="password-input"
             placeholder="******"
-            // onChange={ this.handleChange }
+            onChange={ this.handleChange }
           />
           <button
             type="button"
-            // disabled={ !this.validate(email, password) }
+            disabled={ !this.validate(nome, email, senha) }
             // onClick={ this.handleClick }
           >
             CADASTRAR
