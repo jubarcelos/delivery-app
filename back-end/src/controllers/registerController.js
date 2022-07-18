f896c216bf25991d32006df4d68bc9327edda7b5
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-07-18 18:59:20 -0300

    feat: add md5 to /register service

--- back-end/src/controllers/registerController.js
+++ back-end/src/controllers/registerController.js
@@ -4,7 +4,9 @@ const registerUser = async (req, res) => {
   const payload = req.body;
   try {
     const userCreated = await registerService.registerUser(payload);
-    if (!userCreated) return res.status(409).json({ message: 'Invalid email or password' });
+    if (userCreated.status) {
+      return res.status(userCreated.status).json({ message: userCreated.message });
+    }
     return res.status(201).json(userCreated);
   } catch (err) {
     return res.status(500).send({ message: err.message });
