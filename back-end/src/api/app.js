b28c84c4bfe8b0a027b2a969d54d64bbcafaf32d
italomoraess <italomoraes.sth@gmail.com>
2022-07-19 15:50:17 -0300

    fix: resolve request register

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const registerRoute = require('../routes/registerRoute');
 const loginRoute = require('../routes/loginRoute');
 const customerRoute = require('../routes/customerRoute');
@@ -6,6 +7,7 @@ const customerRoute = require('../routes/customerRoute');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
