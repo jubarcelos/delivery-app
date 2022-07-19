f275a638e265a0b1120d46e995e36ed8f0b502ff
Vanessa <vanessamrios@gmail.com>
2022-07-19 14:46:28 +0200

    refactor: altera o componente Register de classe para funcional

--- front-end/src/pages/register/index.jsx
+++ front-end/src/pages/register/index.jsx
@@ -1,43 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-class Register extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      nome: '',
-      email: '',
-      senha: '',
-      errorMessage: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+function Register() {
+  const history = useHistory();
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  handleClick() {
-    this.setApiData();
-    const response = this.getApiDataFromLocalStorage();
-    if (response.includes(err)) {
-      return this.setState({ errorMessage: 'este e-mail já foi cadastrado' });
+  const validateEmailFormat = () => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validate = () => {
+    const minPasswordSize = 6;
+    const minNameSize = 12;
+    if (validateEmailFormat()
+    && senha.length >= minPasswordSize
+    && nome.length >= minNameSize) {
+      return true;
     }
-    this.history.push('customers/products');
-    // fazer requisição pro backend;
-    // 1. sucesso: 201
-    // pegar token no body e salvar no localStorage
-    // redirecionar para costumer/products
-    // 2. erro: 409
-    // setar a mensagem de erro no erro mensage state.
-    //
-  }
+  };
 
-  handleChange({ target }) {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  setApiData = () => {
-    const { nome, email, senha } = this.state;
+  const setApiData = () => {
     const url = 'https://localhost:3001/register';
     const body = { nome, email, senha };
     fetch(url, {
@@ -47,75 +33,73 @@ class Register extends React.Component {
     }).then((response) => response.json())
       .then((json) => localStorage.setItem('user', JSON.stringify({ json })))
       .catch((err) => console.log(err));
-  }
+  };
 
-  getApiDataFromLocalStorage = () => JSON.parse(localStorage.getItem('user'));
+  const getApiDataFromLocalStorage = () => JSON.parse(localStorage.getItem('user'));
 
-  validateEmailFormat(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(String(email).toLowerCase());
-  }
-
-  validate(nome, email, senha) {
-    const minPasswordSize = 6;
-    const minNameSize = 12;
-    if (this.validateEmailFormat(email)
-    && senha.length >= minPasswordSize
-    && nome.length >= minNameSize) {
-      return true;
+  const handleClick = () => {
+    setApiData();
+    const response = getApiDataFromLocalStorage();
+    if (response.includes(err)) {
+      return setErrorMessage({ errorMessage: 'este e-mail já foi cadastrado' });
     }
-  }
+    history.push('customers/products');
+    // fazer requisição pro backend;
+    // 1. sucesso: 201
+    // pegar token no body e salvar no localStorage
+    // redirecionar para costumer/products
+    // 2. erro: 409
+    // setar a mensagem de erro no erro mensage state.
+    //
+  };
 
-  render() {
-    const { nome, email, senha, errorMessage } = this.state;
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h1>Cadastro</h1>
-        </div>
-        <div>
-          <input
-            type="text"
-            name="nome"
-            value={ nome }
-            data-testid="common_register__input-name"
-            placeholder="Seu nome"
-            onChange={ this.handleChange }
-          />
-          <input
-            type="text"
-            name="email"
-            value={ email }
-            data-testid="common_register__input-email"
-            placeholder="seu-email@site.com.br"
-            onChange={ this.handleChange }
-          />
-          <input
-            type="text"
-            name="senha"
-            value={ senha }
-            data-testid="common_register__input-password"
-            placeholder="******"
-            onChange={ this.handleChange }
-          />
-          <button
-            type="button"
-            disabled={ !this.validate(nome, email, senha) }
-            data-testid="common_register__button-register"
-            onClick={ this.handleClick }
-          >
-            CADASTRAR
-          </button>
-          <span
-            name="common_register__element-invalid_register"
-            hidden={ !errorMessage }
-          >
-            { errorMessage }
-          </span>
-        </div>
+        <h1>Cadastro</h1>
       </div>
-    );
-  }
+      <div>
+        <input
+          type="text"
+          name="nome"
+          value={ nome }
+          data-testid="common_register__input-name"
+          placeholder="Seu nome"
+          onChange={ ({ target }) => setNome(target.value) }
+        />
+        <input
+          type="text"
+          name="email"
+          value={ email }
+          data-testid="common_register__input-email"
+          placeholder="seu-email@site.com.br"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+        <input
+          type="text"
+          name="senha"
+          value={ senha }
+          data-testid="common_register__input-password"
+          placeholder="******"
+          onChange={ ({ target }) => setSenha(target.value) }
+        />
+        <button
+          type="button"
+          disabled={ !validate() }
+          data-testid="common_register__button-register"
+          onClick={ () => handleClick }
+        >
+          CADASTRAR
+        </button>
+        <span
+          name="common_register__element-invalid_register"
+          hidden={ !errorMessage }
+        >
+          { errorMessage }
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
