e07d72650b28e38048486062029de3be172ccafb
mabiiak <mabiane.polniak@gmail.com>
2022-07-19 17:24:25 -0300

    refactor: substitui simulaçao por dados do usuario

--- front-end/src/components/header/index.jsx
+++ front-end/src/components/header/index.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import HeaderStyled from './style';
 
-/*
-  Role:
-  - Pegar o valor de 'role' no localStorage
-  - substituir var roleMock
-
-  Sair:
-  - resetar usuarios no localStorage
-*/
-
 function Header() {
-  const roleMock = 'customer';
-  const userMock = 'user';
+  const getLocalStorage = () => JSON.parse(localStorage.getItem('user'));
+  const { role } = getLocalStorage().user;
+  const { name } = getLocalStorage().user;
+
+  const clearUser = () => {
+    localStorage.setItem('user', JSON.stringify({}));
+  };
 
   return (
     <HeaderStyled>
       <div>
         {
-          roleMock === 'customer'
+          role === 'customer'
             && (
               <div>
                 <h1>
@@ -33,18 +29,18 @@ function Header() {
         }
 
         {
-          roleMock === 'administrator' && (<h1> Gerenciar usuários </h1>)
+          role === 'administrator' && (<h1> Gerenciar usuários </h1>)
         }
         {
-          roleMock !== 'administrator'
-          && roleMock !== 'customer'
+          role !== 'administrator'
+          && role !== 'customer'
           && (<h1> Pedidos </h1>)
         }
       </div>
       <div>
-        <h1>{ userMock }</h1>
+        <h1>{ name }</h1>
         <h1>
-          <Link to="/login">Sair</Link>
+          <Link onClick={ clearUser } to="/login">Sair</Link>
         </h1>
       </div>
     </HeaderStyled>
