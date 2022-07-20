3b70d7c79a4a571e8edccf22d487e27a0deb23eb
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-20 19:28:45 -0300

    feat: cria funções auxiliares de requisição fetch

---
+++ front-end/src/utils/getAPI.js
@@ -0,0 +1,15 @@
+const getApiData = async (rote, send) => {
+  const result = await handleAPI.post('/rote', send)
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
+  return result;
+};
+
+const getApiById = async () => {
+  const result = await handleAPI.post('/', send)
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
+  return result;
+};
+
+export { getApiData, getApiById };
