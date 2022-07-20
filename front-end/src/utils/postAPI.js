cccde0f6e947a988318e62f074d8d49a6ec6a4c1
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-20 18:00:53 -0300

    feat: utils com funções de manipilação do localSotrage e post API
     cria uma função genérica de POST api e salva no localStorage, cria também uma função que apaga as informações no localStorage.

---
+++ front-end/src/utils/postAPI.js
@@ -0,0 +1,18 @@
+const handleAPI = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+// send = {};
+// tag e rote = string;
+
+const setLocalStorageApiData = async (rote, send, tag) => {
+  const result = await handleAPI.post('/rote', send)
+    .then((response) => response.data)
+    .then((data) => localStorage.setItem(tag, JSON.stringify(data)))
+    .catch((error) => console.log(error));
+  return result;
+};
+
+const clearLocalStorage = localStorage.setItem('address', JSON.stringify({}));
+
+export { handleAPI, setLocalStorageApiData, clearLocalStorage };
