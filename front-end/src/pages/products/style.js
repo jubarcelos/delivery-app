b6d85f5f1ac99d4dd2c7eaa7193a22185a5a89a6
mabiiak <mabiane.polniak@gmail.com>
2022-07-20 22:19:12 -0300

    style: cria grid de produtos

---
+++ front-end/src/pages/products/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const GridProducts = styled.div`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+`;
+
+export const Card = styled.div`
+  align-items: center;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+
+  input {
+    text-align: center;
+  }
+`;
