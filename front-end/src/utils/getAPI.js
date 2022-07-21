bcc9d925ac673f87d474d74fde706e7b12eca844
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-21 16:35:04 -0300

    fix: funções  de auxilio para método post e get

--- front-end/src/utils/getAPI.js
+++ front-end/src/utils/getAPI.js
@@ -1,14 +1,14 @@
 import { handleAPI } from './postAPI';
 
-const getApiData = async (rote, send) => {
-  const result = await handleAPI.post('/rote', send)
+const getApiData = async (rote) => {
+  const result = await handleAPI.get(`/${rote}`)
     .then((response) => response.data)
     .catch((error) => console.log(error));
   return result;
 };
 
-const getApiById = async () => {
-  const result = await handleAPI.post('/', send)
+const getApiById = async (rote, id) => {
+  const result = await handleAPI.get(`/${rote}/${id}`)
     .then((response) => response.data)
     .catch((error) => console.log(error));
   return result;
--- front-end/src/utils/postAPI.js
+++ front-end/src/utils/postAPI.js
@@ -8,13 +8,11 @@ const handleAPI = axios.create({
 // tag e rote = string;
 
 const setLocalStorageApiData = async (rote, send, tag) => {
-  const result = await handleAPI.post('/rote', send)
+  const result = await handleAPI.post(`/${rote}`, send)
     .then((response) => response.data)
     .then((data) => localStorage.setItem(tag, JSON.stringify(data)))
     .catch((error) => console.log(error));
   return result;
 };
 
-const clearLocalStorage = localStorage.setItem('address', JSON.stringify({}));
-
-export { handleAPI, setLocalStorageApiData, clearLocalStorage };
+export { handleAPI, setLocalStorageApiData };
