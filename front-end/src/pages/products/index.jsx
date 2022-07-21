59975423580ee466bb717d3029aa2e86ad86f627
mabiiak <mabiane.polniak@gmail.com>
2022-07-20 21:19:22 -0300

    feat: adiciona datatest-id's

--- front-end/src/pages/products/index.jsx
+++ front-end/src/pages/products/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../../components/header';
 
@@ -19,26 +20,52 @@ function Products() {
   return (
     <div>
       <Header />
-      <h1> Products</h1>
       {
         products !== undefined
         && products.map((prod) => (
           <div key={ prod.name }>
-            <p>
+            <p data-testid={ `customer_products__element-card-title-${prod.id}` }>
               { prod.name }
             </p>
-            <p>
-              R$
-              { prod.price }
+            <p data-testid={ `customer_products__element-card-price-${prod.id}` }>
+              { prod.price.replace('.', ',') }
             </p>
             <img
+              data-testid={ `customer_products__img-card-bg-image-${prod.id}` }
               src={ prod.url_image }
               alt={ `imagem do produto ${prod.name}` }
               width="150px"
             />
+            <div>
+              <button
+                type="button"
+                data-testid={ `customer_products__button-card-rm-item-${prod.id}` }
+              >
+                -
+              </button>
+              <input
+                placeholder="0"
+                type="number"
+                data-testid={ `customer_products__input-card-quantity-${prod.id}` }
+                value="0"
+              />
+              <button
+                type="button"
+                data-testid={ `customer_products__button-card-add-item-${prod.id}` }
+              >
+                +
+              </button>
+            </div>
           </div>
         ))
       }
+      <button
+        type="button"
+        data-testid="customer_products__button-cart`"
+        onClick={ (<Redirect to="/customer/checkout" />) }
+      >
+        Ver Carrinho
+      </button>
     </div>
   );
 }
