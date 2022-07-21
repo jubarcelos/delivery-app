e639ff53a7d2f7266700b797f9415c2f615b13e6
mabiiak <mabiane.polniak@gmail.com>
2022-07-21 11:53:37 -0300

    refactor: separa componentes

--- front-end/src/pages/products/style.js
+++ front-end/src/pages/products/style.js
@@ -1,20 +1,10 @@
 import styled from 'styled-components';
 
-export const GridProducts = styled.div`
+const GridProducts = styled.div`
   align-items: center;
   display: flex;
   flex-wrap: wrap;
   justify-content: left;
 `;
 
-export const Card = styled.div`
-  align-items: center;
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  margin: 10px;
-
-  input {
-    text-align: center;
-  }
-`;
+export default GridProducts;
