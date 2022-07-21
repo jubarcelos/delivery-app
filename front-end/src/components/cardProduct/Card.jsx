47effc0e5f46126fe207765f56992c384f0bbb11
mabiiak <mabiane.polniak@gmail.com>
2022-07-21 18:24:23 -0300

    feat: passa itens no carrinho para estado

--- front-end/src/components/cardProduct/Card.jsx
+++ front-end/src/components/cardProduct/Card.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import CardStyle from './style';
+import Context from '../../context';
 
 function Card({ produto }) {
+  const { setCart } = useContext(Context);
   const [countClick, setCountClick] = useState(0);
   const [totalPrice, setPrice] = useState(0);
 
@@ -14,8 +16,17 @@ function Card({ produto }) {
     setCountClick(countClick - 1);
   };
 
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setCountClick(value);
+  };
+
   useEffect(() => {
     setPrice((countClick * produto.price).toFixed(2));
+
+    if (countClick <= 0) {
+      setCountClick(0);
+    }
   }, [countClick]);
 
   useEffect(() => {
@@ -24,23 +35,29 @@ function Card({ produto }) {
       id: produto.id,
       qty: countClick,
       price: produto.price,
-      totalPrice,
+      sumItem: (countClick * produto.price).toFixed(2),
     };
 
     const local = JSON.parse(localStorage.getItem('cart'));
 
     if (local === null && countClick > 0) {
       localStorage.setItem('cart', JSON.stringify([infos]));
+      const newLocal = JSON.parse(localStorage.getItem('cart'));
+      setCart(newLocal);
     }
 
     if (local !== null && countClick > 0) {
       const filterItem = local.filter((prod) => prod.id !== produto.id);
       localStorage.setItem('cart', JSON.stringify([...filterItem, { ...infos }]));
+      const newLocal = JSON.parse(localStorage.getItem('cart'));
+      setCart(newLocal);
     }
 
     if (local !== null && countClick < 1) {
       const removeItem = local.filter((prod) => prod.id !== produto.id);
       localStorage.setItem('cart', JSON.stringify(removeItem));
+      const newLocal = JSON.parse(localStorage.getItem('cart'));
+      setCart(newLocal);
     }
   }, [totalPrice]);
 
@@ -72,7 +89,7 @@ function Card({ produto }) {
           placeholder="0"
           type="number"
           data-testid={ `customer_products__input-card-quantity-${produto.id}` }
-          min="0"
+          onChange={ handleChange }
           value={ countClick }
         />
         <button
