ea493fc5901d86e2dbd2fbd7217c0a4e835d3454
mabiiak <mabiane.polniak@gmail.com>
2022-07-22 17:24:00 -0300

    fix: corrige rota no back

--- back-end/src/routes/customerRoute.js
+++ back-end/src/routes/customerRoute.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 
 routes.get('/products', customerController.getAll);
 routes.get('/products/:id', customerController.getById);
-routes.get('/orders/', customerController.getAllOrders);
+routes.get('/orders', customerController.getAllOrders);
 routes.get('/orders/:id', customerController.getByIdOrders);
 routes.post('/orders', checkAuth.jwtValidator, customerController.postOrder);
 
