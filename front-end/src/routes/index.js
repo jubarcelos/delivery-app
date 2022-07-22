70846f591f3ac47476ee5fc61f27c31b9f1fad39
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-21 23:03:52 -0300

    feat:Implementa rota e page OrderId

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -24,6 +24,7 @@ export default class Routes extends Component {
             <Route exact path="/customer" component={ Customers } />
             <Route exact path="/customer/products" component={ Products } />
             <Route exact path="/customer/orders" component={ CustomerOrders } />
+            <Route exact path="/customer/orders/:id" component={ CustomerOrders } />
             <Route exact path="/customer/checkout" component={ Checkout } />
             <Route exact path="/seller" component={ Saller } />
             <Route exact path="/seller/orders" component={ AllOrders } />
