df9d7d121f453da2aee6130d258c57b7448a015e
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-26 21:13:41 -0300

    refactor: mudança da rota e atualização de componentes na page admin

--- front-end/src/pages/admin/index.jsx
+++ front-end/src/pages/admin/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from '../../components/header';
+import RegisterForm from '../../components/registerForm';
 
 function Admin() {
-  return (<h1> Admin</h1>);
+  const route = 'admin/manage';
+
+  return (
+    <>
+      <Header />
+      <h1>FALA BRUNO</h1>
+      <RegisterForm
+        formType="admin_manage"
+        route={ route }
+      />
+    </>
+  );
 }
 
 export default Admin;
