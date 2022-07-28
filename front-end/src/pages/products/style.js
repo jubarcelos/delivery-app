57dfcf62c9a1db0dcff43f054d0a66555efb07ea
mabiiak <mabiane.polniak@gmail.com>
2022-07-28 16:39:04 -0300

    style: estiliza botão 'ver carrinho'

--- front-end/src/pages/products/style.js
+++ front-end/src/pages/products/style.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
 
-const GridProducts = styled.div`
+export const GridProducts = styled.div`
   align-items: center;
   display: flex;
   flex-wrap: wrap;
   justify-content: initial;
-`;
+  `;
 
-export default GridProducts;
+export const ButtonTotalStyles = styled.button`
+  background-color: #036B52;
+  border-radius: 3px;
+  border: solid 1px #b5b5b5;
+  bottom: 12px;
+  color: white;
+  font-size: 20px;
+  height: 55px;
+  position: fixed;
+  right: 19px;
+  width: 20%;
+`;
